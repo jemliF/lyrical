4d778e7faa01f34b24d2fcdf9ad24a41adc7cca7
jemliF <jemlifathi2013@gmail.com>
2017-08-29 19:04:44 +0100

    section1

--- server/server.js
+++ server/server.js
@@ -8,22 +8,20 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://root:password@ds111124.mlab.com:11124/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+mongoose
+  .connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use('/graphql', expressGraphQL({schema, graphiql: true}));
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
